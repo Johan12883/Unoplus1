6ce90af4b0993f8fdfc543d641860ca94717605d
Johan12883 <garcia.johan@uabc.edu.mx>
2022-12-08 20:05:09 -0800

    Incia solo una ventana vacia

--- Proyecto U +1!/main.cpp
+++ Proyecto U +1!/main.cpp
@@ -1,8 +1,19 @@
 #include <allegro5/allegro.h>
+#include <allegro5/allegro_native_dialog.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
+	if (!al_init())
+	{
+		al_show_native_message_box(NULL, "ERROR", "ERROR 404", "El programa no pudo cargar correctamente", NULL, ALLEGRO_MESSAGEBOX_ERROR);
+	}
+	
+	ALLEGRO_DISPLAY* main = al_create_display(720, 480);
 
+	while (true)
+	{
+		al_flip_display();
+	}
 }
