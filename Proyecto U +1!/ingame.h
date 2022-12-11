06f92d62d05529412b4d2aaebf6c41b7f4646e86
Johan12883 <garcia.johan@uabc.edu.mx>
2022-12-10 23:34:05 -0800

    Menu hecho, faltan img

---
+++ Proyecto U +1!/ingame.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <stdlib.h>
+#include <stdio.h>
+#include <allegro5/allegro5.h>
+#include <allegro5/allegro_image.h>
+
--- Proyecto U +1!/main.cpp
+++ Proyecto U +1!/main.cpp
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 #include <Windows.h>
 #include <string>
+#include "ingame.h"
 
 int main()
 {
@@ -16,6 +17,9 @@ int main()
 	int alto = 580;
 	int x = -1, y = -1;
 	int seg = 0;
+	// Jugar, Opciones, Salir
+	int btn[] = { 0,0,0 };
+
 	if (!al_init())
 	{
 		al_show_native_message_box(NULL, "ERROR", "ERROR 404", "El programa no pudo cargar correctamente", NULL, ALLEGRO_MESSAGEBOX_ERROR);
@@ -34,9 +38,9 @@ int main()
 	//Codigo para cargar imagenes
 	al_init_image_addon();
 	ALLEGRO_BITMAP* menu_null = al_load_bitmap("menu_temp.PNG");
-	
+
 	ALLEGRO_TIMER* segTimer = al_create_timer(1.0);
-	ALLEGRO_EVENT evento;	
+	ALLEGRO_EVENT evento;
 	ALLEGRO_EVENT_QUEUE* queue = al_create_event_queue();
 
 	al_register_event_source(queue, al_get_timer_event_source(segTimer));
@@ -45,6 +49,7 @@ int main()
 
 	al_init_primitives_addon();
 	ALLEGRO_COLOR blanco = al_map_rgb(255, 255, 255);
+	ALLEGRO_COLOR negro = al_map_rgb(0, 0, 0);
 
 
 
@@ -60,7 +65,7 @@ int main()
 		}
 		//Las capas aqui funcionan en orden ascendente
 		al_draw_bitmap(menu_null, 0, 0, 0);		//Esta es la capa 1, la parte de hasta abajo (esta funcion carga la imagen en la pantalla)
-		al_draw_text(pixel_font, al_map_rgb(0, 0, 0), ancho/2, 100, 
+		al_draw_text(pixel_font, al_map_rgb(0, 0, 0), ancho / 2, 100,
 			ALLEGRO_ALIGN_CENTER, "UNO + 1!");		// Y esta es la capa 2, que esta arriba de la capa 1
 
 		if (evento.type == ALLEGRO_EVENT_MOUSE_AXES || evento.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN)
@@ -68,14 +73,80 @@ int main()
 			//Posicion del mouse
 			x = evento.mouse.x;
 			y = evento.mouse.y;
+			printf("x: %d, y: %d\n", x, y);
 
-			// Click del mouse (& 1 para el boton izquierdo, & 2 para el boton derecho y & 4 para la rueda)
-			if (x >= 180 && x <= 390 && y >= 220 && y <= 300 && evento.mouse.button & 1)
+			//Boton jugar
+			if (x >= 180 && x <= 390 && y >= 220 && y <= 300)
 			{
-				al_draw_filled_rectangle(180, 220, 390, 300, blanco);
-				printf("x: %d, y: %d\n", x, y);
+				btn[0] = 1;
+				// Click del mouse (& 1 para el boton izquierdo, & 2 para el boton derecho y & 4 para la rueda)
+				if (evento.mouse.button & 1)
+				{
+					printf("x: %d, y: %d\n", x, y); //Aca entrará a la funcion para jugar
+				}
+				else
+				{
+					btn[0] = 0;
+				}
 			}
+
+			//Boton opciones
+			if (x >= 180 && x <= 390 && y >= 326 && y <= 405)
+			{
+				btn[1] = 1;
+				if (evento.mouse.button & 1)
+				{
+					printf("x: %d, y: %d\n", x, y); //Aca entrará a la funcion para las opciones
+				}
+				else
+				{
+					btn[1] = 0;
+				}
+			}
+
+			//Boton salir
+			if (x >= 180 && x <= 390 && y >= 432 && y <= 513)
+			{
+				btn[2] = 1;
+				if (evento.mouse.button & 1)
+				{
+					printf("x: %d, y: %d\n", x, y); //Aca saldra del juego
+					return 1;
+				}
+				else
+				{
+					btn[2] = 0;
+				}
+			}
+
+			if (x >= 180 && x <= 390 && y >= 220 && y <= 300)
+			{							//Cordenadas (x1,y1) y (x2,y2)
+				al_draw_filled_rectangle(180, 220, 390, 300, blanco); //Aqui ira el menu donde se resaltara el boton "Jugar"
+			}
+			else
+			{							//Cordenadas (x1,y1) y (x2,y2)
+				al_draw_filled_rectangle(180, 220, 390, 300, negro); //Aqui ira el menu "NULL"
+
+			}
+			if (x >= 180 && x <= 390 && y >= 326 && y <= 405)
+			{							//Cordenadas (x1,y1) y (x2,y2)
+				al_draw_filled_rectangle(180, 326, 390, 405, blanco); //Aqui ira el menu donde se resaltara el boton "Opciones"
+			}
+			else
+			{							//Cordenadas (x1,y1) y (x2,y2)
+				al_draw_filled_rectangle(180, 326, 390, 405, negro); //Aqui ira el menu "NULL"
+			}
+			if (x >= 180 && x <= 390 && y >= 432 && y <= 513)
+			{							//Cordenadas (x1,y1) y (x2,y2)
+				al_draw_filled_rectangle(180, 432, 390, 513, blanco); //Aqui ira el menu donde se resaltara el boton "Salir"
+			}
+			else
+			{							//Cordenadas (x1,y1) y (x2,y2)
+				al_draw_filled_rectangle(180, 432, 390, 513, negro); //Aqui ira el menu "NULL"
+			}
+
 		}
 		al_flip_display();
+
 	}
 }
